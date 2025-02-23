141c8cc6cb73f6aba3bccd25801ac11f38ee37c2
MiroPletscher <115536403+MiroPletscher@users.noreply.github.com>
2025-02-23 12:34:37 +0700

    Initial Commit

---
+++ Gravitas Lab/Gravitas_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gravitas_LabApp.swift
+//  Gravitas Lab
+//
+//  Created by Miro Pletscher on 23/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Gravitas_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
